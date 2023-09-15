03df0ed3ca18f3ee7370b5c6f897dea8179d936b
unknown <ali1baktash2@gmail.com>
2023-09-15 16:18:21 +0330

    first session mid

--- reactj1/src/main.jsx
+++ reactj1/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import {Home} from './views/Home.jsx'
+import Home from './views/Home.jsx'
 
 const App=()=>{
   // return React.createElement("h1",{},React.createElement("p",{},"hello"))
@@ -7,10 +9,19 @@ const App=()=>{
   //jsx
   //return hello
   // make component for a navbar with jsx
+  function helloConsole(a){
+    return a
+  }
   return (
-    <div>
-      <h1>Hello</h1>
+    <React.Fragment>
+      <div>
+      <h1 className='htag'>Hello{1+2}</h1>
+      <br />
+      <button onClick={()=>helloConsole(1)}>btn {helloConsole(2)}</button>
     </div>
+    <h1>hello world</h1>
+    <Home/>
+    </React.Fragment>
   );
   
 }
---
+++ reactj1/src/views/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// export const Home = () => (
+//   <>
+//     <div>
+//       <h1>Home</h1> 
+//       <p>lorem</p>
+//     </div>
+//   </>
+// );
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1> 
+      <p>lorem</p>
+    </div>
+  );
+  }
+// export default Home;
+// way one for export va dar in rah ba har esmi an taraf
